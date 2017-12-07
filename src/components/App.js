12ebc0840c1da622dab7bf82945ceb221804de1d
yazeedb <ybzadough@gmail.com>
2017-12-06 21:17:07 -0500

    update todos

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AddTodo from './AddTodo';
 
 const style = {
@@ -8,9 +8,35 @@ const style = {
 	width: '66%'
 };
 
-export default () => (
-	<div className="app" style={ style }>
-		<h1>Todo List</h1>
-		<AddTodo />
-	</div>
-);
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.addTodo = this.addTodo.bind(this);
+
+		this.state = {
+			todos: []
+		};
+	}
+
+	addTodo(value) {
+		console.log('addTodo got value:', value);
+
+		const { todos } = this.state;
+
+		this.setState({ todos: [...todos, value] }, () => {
+			console.log('my todos:', this.state.todos);
+		});
+	}
+
+	render() {
+		return (
+			<div className="app" style={ style }>
+				<h1>Todo List</h1>
+				<AddTodo
+					handleSubmit={ this.addTodo }
+				/>
+			</div>
+		);
+	}
+}
