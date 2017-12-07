18c361d30ae3c03342dbac6744931b60e09bf9c4
yazeedb <ybzadough@gmail.com>
2017-12-06 20:16:15 -0500

    add styles

--- src/components/AddTodo/index.js
+++ src/components/AddTodo/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+const style = {
+	fontSize: '1.5rem'
+};
+
 export default () => (
-	<input type="text" placeholder="Add a todo..." />
+	<input type="text" placeholder="Add a todo..." style={ style } />
 );
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import AddTodo from './AddTodo';
 
+const style = {
+	textAlign: 'center',
+	maxWidth: '960px',
+	margin: '0 auto',
+	width: '66%'
+};
+
 export default () => (
-	<div className="app">
+	<div className="app" style={ style }>
 		<h1>Todo List</h1>
 		<AddTodo />
 	</div>
