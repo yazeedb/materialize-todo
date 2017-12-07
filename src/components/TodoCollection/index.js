878b3d3433fe734b6ac8e5b46953f5f6b75aaceb
yazeedb <ybzadough@gmail.com>
2017-12-07 05:25:25 -0500

    move key to <li>

--- src/components/TodoCollection/index.js
+++ src/components/TodoCollection/index.js
@@ -5,12 +5,11 @@ const makeTodoCollection = (todos) => (
 	<ul className="todos collection">
 		{
 			todos.map(({ id, name, completed }) => (
-				<li className="collection-item">
+				<li className="collection-item" key={ id }>
 					<Todo
 						id={ id }
 						name={ name }
 						completed={ completed }
-						key={ id }
 					/>
 				</li>
 			))
