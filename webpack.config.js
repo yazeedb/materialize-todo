cca13666b9f96b8009be3e584c1f0f2c5dab8b22
yazeedb <ybzadough@gmail.com>
2018-01-14 15:16:28 -0500

    add .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -28,13 +28,7 @@ module.exports = {
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/preset-react'],
-						plugins: ['@babel/plugin-proposal-object-rest-spread']
-					}
-				}
+				use: 'babel-loader'
 			},
 			{
 				test: /\.scss$/,
